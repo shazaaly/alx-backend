2d4b85121f5131dbf06b3097fa259c8f5edbcba4
shazaaly <shaza.aly@gmail.com>
2024-01-26 22:10:57 +0200

    // (Floor Division Operator)

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -81,6 +81,6 @@ class Server:
             "data": data,
             "next_page": page + 1 if page >= 0 else None,
             "prev_page": page - 1 if page >= 1 else None,
-            "total_pages": math.ceil(len(self.dataset()) / page_size)
+            "total_pages": math.ceil(len(self.dataset()) // page_size)
             }
         return dict
