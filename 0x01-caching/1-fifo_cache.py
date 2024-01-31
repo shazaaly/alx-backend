4cf6c186768d9b94577d60ea2a3e9dd2632994ac
shazaaly <shaza.aly@gmail.com>
2024-01-31 13:13:12 +0200

    p

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -3,8 +3,8 @@
 """
 
 
-BaseCaching = __import__('base_caching').BaseCaching
 from collections import OrderedDict
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class FIFOCache(BaseCaching):
@@ -13,25 +13,22 @@ class FIFOCache(BaseCaching):
     def __init__(self):
         super().__init__()
         self.cache_data = OrderedDict()
-    
+          
     def put(self, key, item):
         """ put key value pair into cache"""
         if (not key or not item):
             return
         capacity = BaseCaching.MAX_ITEMS
         discarded = None
-
         if key not in self.cache_data:
             self.cache_data[key] = item
         if len(self.cache_data) > capacity:
             discarded = self.cache_data.popitem(last=False)
         print(f"DISCARD: {discarded}\n")
         
-        
-        
     def get(self, key):
         """ return the value in self.cache_data linked to key."""
-        
         if (key is None or key not in self.cache_data):
             return none
         return self.cache_data.get(key)
+    
