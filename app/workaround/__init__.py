b72f9af26ca594d082e1f6e525a90e9ca84e1f5c
shazaaly <shaza.aly@gmail.com>
2024-02-06 19:08:51 +0200

    Add language selection based on Accept-Language header and display localized time on home page

--- app/workaround/__init__.py
+++ app/workaround/__init__.py
@@ -1,10 +1,25 @@
-from flask import Flask, request
-from flask_babel import Babel
+# from flask import Flask, request
 
+# from flask_babel import Babel, get_locale
+# app = Flask(__name__)
+
+# babel = Babel(app)
+
+# @babel.localeselector
+# def get_locale():
+#     # Automatically use best match from the Accept-Language header
+#     return request.accept_languages.best_match(['en', 'es', 'fr', 'de'])
+
+
+from flask import Flask, request,render_template
+
+from flask_babel import format_datetime
+from datetime import datetime
 app = Flask(__name__)
-babel = Babel(app)
 
-@babel.localeselector
-def get_locale():
-    # Attempt to get the language from the URL parameters
-    return request.args.get('lang', default='en')
+
+@app.route('/')
+def home():
+    now = datetime.utcnow()
+    localized_time = format_datetime(now, 'yyyy-MM-dd HH:mm:ssZZZZ', locale='auto')
+    return render_template('home.html', localized_time=localized_time)
