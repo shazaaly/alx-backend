ea5e4fac241bc39c1682ef41eb9dc733e77738fa
shazaaly <shaza.aly@gmail.com>
2024-02-07 11:50:24 +0200

    Refactor Flask app configuration and route handling

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,52 +1,44 @@
 #!/usr/bin/env python3
-""" A script for basic flask integration"""
+"""
+Flask app
+"""
+from flask import (
+    Flask,
+    render_template,
+    request
+)
+from flask_babel import Babel
 
-from flask import Flask, render_template, request
-from flask_babel import Babel, gettext
+
+class Config(object):
+    """
+    Configuration for Babel
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
-babel = Babel(app)
-app.jinja_env.autoescape = True
-
-
-class Config:
-    """
-    Configuration class for the application.
-        Attributes:
-        Languages (list): List of supported languages.
-    """
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-    BABEL_DEFAULT_LOCALE = 'en'
-
-
 app.config.from_object(Config)
+babel = Babel(app)
 
 
-# @babel.localeselector
+@babel.localeselector
 def get_locale():
     """
-    Get the best matching language from the request's accepted languages.
-
-    :return: The best matching language.
+    Select and return best language match based on supported languages
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-babel.init_app(app, locale_selector=get_locale)
-
-
-@app.route("/")
-def hello():
-    """This function returns the rendered
-    template for the index.html page.
-    Returns:
-        The rendered template for the
-        index.html page.
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Handles / route
     """
     return render_template('3-index.html')
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
