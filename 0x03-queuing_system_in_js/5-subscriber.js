dc4411903a6e4c42bc0b76b1400fb3cee0b84ea6
shazaaly <shaza.aly@gmail.com>
2024-03-05 01:41:53 +0200

    Refactor Redis client initialization and error handling

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -1,11 +1,21 @@
-import { createClient, print } from 'redis';
+import { createClient } from 'redis';
 
 const redisClient = createClient();
 
-redisClient.on('connect', function() {
-  console.log('Redis client connected to the server');
+redisClient.on('connect', function () {
+    console.log('Redis client connected to the server');
 });
 
-redisClient.on('error', function(error) {
-  console.log(`Redis client not connected to the server: ${error}`);
+redisClient.on('error', function (error) {
+    console.log(`Redis client not connected to the server: ${error.message}`);
+});
+
+redisClient.subscribe('holberton school channel');
+
+redisClient.on('message', function (channel, message) {
+  console.log(`${message}`);
+  if (message === 'KILL_SERVER') {
+    redisClient.unsubscribe('holberton school channel');
+    redisClient.end(true);
+  }
 });
